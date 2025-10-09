f2c6b225315eaee5ac6680f70572b62e765b5a4d
shasank-13 <shasankthupakula@gmail.com>
2025-10-09 10:42:29 +0530

    Update iot 2-1.py

--- iot 2-1.py
+++ iot 2-1.py
@@ -1,19 +1,23 @@
-# Area of circle using oops
+# Area of circle using OOP
 import math
-class circle:
-    def __init__(self,r):
-        self.r = r
-        
-        def calarea(self):
-            return math.pi * self.r**2
-        def cirper(self):
-            return 2 * math.pi * self.r
 
-#driver code
-r= float(input("input the radius of the circle: "))
+class circle:
+    def __init__(self, r):
+        self.r = r
+    
+    def calarea(self):
+        return math.pi * self.r ** 2
+    
+    def cirper(self):
+        return 2 * math.pi * self.r
+
+# driver code
+r = float(input("Input the radius of the circle: "))
 
 a = circle(r)
 
 area = a.calarea()
+perimeter = a.cirper()
 
-print("Area of circle:",area)
+print("Area of circle:", area)
+print("Perimeter of circle:", perimeter)
